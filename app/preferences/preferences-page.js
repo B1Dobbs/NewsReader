a2dc081360847a3aac05551afb889e5f04da344a
B1Dobbs <brookedobbins5@gmail.com>
2019-02-17 00:24:10 -0600

    Beginning of Dark Theme

--- app/preferences/preferences-page.js
+++ app/preferences/preferences-page.js
@@ -3,39 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var preferences_view_model_1 = require("./preferences-view-model");
 const frame = require("tns-core-modules/ui/frame");
 const observableModule = require("tns-core-modules/data/observable");
+var page = null;
 // Event handler for Page "pageLoaded" event attached in home-page.xml
 function pageLoaded(args) {
-    var page = args.object;
+    page = args.object;
     page.bindingContext = new preferences_view_model_1.PreferencesViewModel();
 }
 
 function pageNavigateTo(args) {
-    var page = args.object;
-    const vm = new observableModule.Observable();
-    vm.set("firstSwitchResult", true);
-    vm.set("firstSwitch", true);
-    vm.set("secondSwitchResult", false);
-    vm.set("secondSwitch", false);
-    vm.set("thirdSwitchResult", true);
-    vm.set("thirdSwitch", true);
-    // handling Switch checked change
-    vm.on(observableModule.Observable.propertyChangeEvent, (propertyChangeData) => {
-        switch (propertyChangeData.propertyName) {
-            case "firstSwitch":
-                vm.set("firstSwitchResult", propertyChangeData.value);
-                break;
-            case "secondSwitch":
-                vm.set("secondSwitchResult", propertyChangeData.value);
-                break;
-            case "thirdSwitch":
-                vm.set("thirdSwitchResult", propertyChangeData.value);
-                break;
-
-            default:
-                break;
-        }
-    });
-    page.bindingContext = vm;
+    page = args.object;
+    page.bindingContext = new preferences_view_model_1.PreferencesViewModel();
     //page.bindingContext = new preferences_view_model_1.PreferencesViewModel();
 }
 
@@ -44,7 +21,9 @@ function gotoHome(args) {
 }
 
 function gotoSearch(args) {
-    frame.topmost().navigate( 'search/search-page' );
+    frame.topmost().navigate({ 
+        moduleName: "search/search-page",
+        bindingContext: page.bindingContext});
 }
 
 exports.gotoSearch = gotoSearch;
--- app/preferences/preferences-view-model.js
+++ app/preferences/preferences-view-model.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var observable_property_decorator_1 = require("../observable-property-decorator");
 var observable_1 = require("tns-core-modules/data/observable");
 var platform_1 = require("tns-core-modules/platform");
-
+var application = require("tns-core-modules/application");
 
 var PreferencesViewModel = /** @class */ (function (_super) {
     
@@ -11,6 +11,26 @@ var PreferencesViewModel = /** @class */ (function (_super) {
     function PreferencesViewModel() {
         var _this = _super.call(this) || this;
 
+        this.set("firstSwitch", false);
+        //this.set("page_theme", "dark_theme");
+        // handling Switch checked change
+        this.on(observable_1.Observable.propertyChangeEvent, (propertyChangeData) => {
+            if(propertyChangeData.propertyName == "firstSwitch"){
+                if(propertyChangeData.value){
+                    //this.app.applyDarkTheme();
+                    //var theme_string = this.app.getTheme();
+                    application.setCssFileName("app-dark.css")
+                    this.set("page_theme", "dark_theme");
+                    this.set("text", "text_light");
+                }
+                else{
+                    application.setCssFileName("app.css");
+                    this.set("page_theme", "light_theme");
+                    this.set("text", "text_dark");
+                }
+            }
+            
+        });
 
         return _this;
     }
